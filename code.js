6dde26fa1b249c806d04306d02cea6a123626c4e
gary <gary.white@lodgit.com>
2021-11-09 13:14:48 +0100

    code working

---
+++ code.js
@@ -0,0 +1,8 @@
+function stringToArray(string){
+var myArr = string.split(' ')
+return myArr;
+}
+
+
+console.log(stringToArray("Robin Singh")); // return:  ["Robin", "Singh"]
+console.log(stringToArray("I love arrays they are my favorite")); // Return ["I", "love", "arrays", "they", "are", "my", "favorite"]
